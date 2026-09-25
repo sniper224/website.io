94630b6f36bcb152ca3ad31eaa3fe1d157ace898
sniper224 <160593170+sniper224@users.noreply.github.com>
2026-09-25 14:53:23 +0000

    penis

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template, request
+usernames_passwords = {
+    'user1': 'password1',
+    'user2': 'password2',
+    'user3': 'password3'
+}
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('navigation.xhtml',store_name='M&S')
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    return render_template('login.xhtml')
 
+@app.route('/login_val', methods=['POST'])
+def login_val():
+    name = request.form.get('username')
+    password = request.form.get('password')
+    if name in usernames_passwords and usernames_passwords[name] == password:
+        return render_template('home.xhtml')  
+    else:
+        return render_template('login.xhtml')       
 if __name__ == '__main__':
     app.run(debug=True)
